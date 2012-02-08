f27a8db14ca4862c94aa793a56543d6d548a8c33
Nicolae Tiut <nicolae.tiut@threepillarglobal.com>
2012-02-08 12:09:29 +0200

    Modified the 2 problems according to Sever's feedback

--- checkpoint1/merge.py
+++ checkpoint1/merge.py
@@ -1,3 +1,5 @@
+import operator
+
 def merge(a, b):
     """Merge 2 objects with any depth (including contained dictionaries,
     lists, sets, strings, integers,floats). Type mismatches should yield
@@ -23,34 +25,76 @@ def merge(a, b):
 
     >>> merge({}, {})
     {}
+
+    >>> a = {
+    ... 'x': [1, 2, 3], 'y': 1, 'z': set([1, 2, 3]),
+    ... 'w': 'qweqwe', 't': {'a': [1, 2]}, 'm': [1]
+    ... }
+    >>> b = {
+    ... 'x': [4, 5, 6], 'y': 4, 'z': set([4, 2, 3]),
+    ... 'w': 'asdf', 't': {'a': [3, 2]}, 'm': "wer"
+    ... }
+    >>> r = merge(a, b)
+
+    >>> r['m']
+    ([1], 'wer')
+
+    >>> sorted(r['t'].items())
+    [('a', [1, 2, 3, 2])]
+
+    >>> r['w']
+    'qweqweasdf'
+
+    >>> r['y']
+    5
+
+    >>> r['x']
+    [1, 2, 3, 4, 5, 6]
+
+    >>> r['z']
+    set([1, 2, 3, 4])
+
+    >>> sorted(merge({'x': 1}, {'y': 2}).items())
+    [('x', 1), ('y', 2)]
+
     """
-    result = {}
-    for property, value in a.iteritems():
-        try:
-            result[property] = merge(value, b[property])
-        except AttributeError:
-            if type(value) is set:
-                result[property] = value | b[property]
-            else:
-                try:
-                    result[property] = value + b[property]
-                except TypeError:
-                    result[property] = (value, b[property])
+
+    process_chain = try_to_apply_operator_in_order(
+        operator.add,
+        operator.or_,
+        merge,
+        lambda a, b: (a, b)
+    )
+
+    result = dict(a)
+    result.update(b)
+
+    a_key_set = set(a.keys())
+    b_key_set = set(b.keys())
+    common_keys = a_key_set.intersection(b_key_set)
+
+    for key in common_keys:
+        result[key] = process_chain(a[key], b[key])
+
     return result
 
 
-if __name__ == "__main__":
+def try_to_apply_operator_in_order(operation, next_operation=None):
 
-    a = {
-       'x': [1, 2, 3], 'y': 1, 'z': set([1, 2, 3]),
-       'w': 'qweqwe', 't': {'a': [1, 2]}, 'm': [1]
-    }
+    def wrapper(a, b):
+        try:
+            return operation(a, b)
+        except TypeError:
+            if callable(next_operation):
+                return next_operation(a, b)
+            else:
+                raise
 
-    b = {
-       'x': [4, 5, 6], 'y': 4, 'z': set([4, 2, 3]),
-       'w': 'asdf', 't': {'a': [3, 2]}, 'm': "wer"
-    }
+    return wrapper
 
-    print "First object is: ", a
-    print "Second object is: ", b
-    print "Merged object is: ", merge(a, b)
+
+def try_chain(*operations):
+    chain = try_to_apply_operator_in_order(operations[-1])
+    for op in reversed(operations[:-1]):
+        chain = try_to_apply_operator_in_order(op, chain)
+    return chain
--- checkpoint2/decorator.py
+++ checkpoint2/decorator.py
@@ -10,25 +10,90 @@ import time
 now = datetime.datetime.now()
 
 
-def time_slow(threshold=None):
+def get_current_millis():
+    return int(round(time.time() * 1000))
+
+
+def get_operation_results_in_order(operations):
+    """ Return results of operations called in order.
+
+    >>> def get_current_millis():
+    ...     return int(round(time.time() * 1000))
+    >>> def aFunction():
+    ...     for x in range(1, 1000000):
+    ...         y = x * x
+    >>> op1 = {'call': get_current_millis, 'args': None, 'kwargs': None}
+    >>> op2 = {'call': aFunction, 'args': None, 'kwargs': None}
+    >>> operations = [op1, op2, op1]
+    >>> results = get_operation_results_in_order(operations)
+    >>> len(results)
+    3
+
+    >>> operations = [None, None, op2]
+    >>> results = get_operation_results_in_order(operations)
+    >>> len(results)
+    0
+    """
+    results = []
+    for op in operations:
+        if op and callable(op['call']):
+            if op['args'] and op['kwargs']:
+                results.append(
+            op['call'](*op['args'], **op['kwargs'])
+            )
+            else:
+                results.append(op['call']())
+        else:
+            return []
+    return results
+
+
+def check_millis(time_before, time_after, threshold):
+    """ Checks if interval between time_before and time_after > than threshold.
+
+    >>> check_millis(1, 2, 3)
+    False
+
+    >>> check_millis(1, 6, 3)
+    True
+
+    >>> check_millis(4, 1, 3)
+    False
+
+    >>> check_millis(4, 1, -5)
+    False
+    """
+    if (time_after > time_before and time_after - time_before > threshold):
+        return True
+    return False
+
+
+def time_slow(logger, threshold=None):
     def my_decorator(aFunction):
         def wrapped(*args, **kwargs):
-            millis1 = int(round(time.time() * 1000))
-            res = aFunction(*args, **kwargs)
-            millis2 = int(round(time.time() * 1000))
-            total_time = millis2 - millis1
-            if (total_time > threshold):
-                print "The function call to '" +
+            op1 = {'call': get_current_millis, 'args': None, 'kwargs': None}
+            op2 = {'call': aFunction, 'args': args, 'kwargs': kwargs}
+            operations = [op1, op2, op1]
+            res = get_operation_results_in_order(operations)
+            if len(res) == 3 and check_millis(res[0], res[2], threshold):
+                logger.log("The function call to '" +
                 aFunction.__name__ +
-                "' lasted " + str(total_time) + " milliseconds."
-                print "Logging slow function called at " +
-                now.strftime("%Y-%m-%d %H:%M") + "."
-            return res
+                "' lasted " + str(res[2] - res[0]) + " milliseconds.")
+                logger.log("Logging slow function called at " +
+                now.strftime("%Y-%m-%d %H:%M") + ".")
         return wrapped
     return my_decorator
 
 
-@time_slow(threshold=10)
+class MyLogger:
+
+    def log(self, message):
+        print message
+
+logger = MyLogger()
+
+
+@time_slow(logger, threshold=10)
 def aFunction():
     for x in range(1, 1000000):
         y = x * x
